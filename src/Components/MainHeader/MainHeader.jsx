6854b044b36879744da611b0028d1b7ff54a65f5
MLeys <102929892+MLeys@users.noreply.github.com>
2023-03-18 16:34:46 -0400

    BUILD: option menu links

--- src/Components/MainHeader/MainHeader.jsx
+++ src/Components/MainHeader/MainHeader.jsx
@@ -17,22 +17,16 @@ import Stack from '@mui/material/Stack';
 import theme from '../../theme';
 
 const sections = [
-  {
-    title: "About",
-    link: "#intro",
-  },
-  {
-    title: "Technologies",
-    link: "#tech"
-  },
-  {
-    title: "Projects",
-    link: "#projects"
-  },
+  { title: "About", link: "#intro" },
+  { title: "Technologies", link: "#tech" },
+  { title: "Projects", link: "#projects" }
 ]
 
+const options = [
+  { title: 'Resume', link: '#'},
+  { title: 'Contact', link: '#contact'}
+]
 
-const info = ['Resume', 'Contact', ];
 
 function MainHeader() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -64,15 +58,18 @@ function MainHeader() {
       <Container maxWidth="100%">
         <Toolbar disableGutters>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="More Information"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-            >
-              <MenuIcon  />
-            </IconButton>
+            <Tooltip title='Menu'>
+              <IconButton
+                size="large"
+                aria-label="More Information"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+              >
+                <MenuIcon  />
+              </IconButton>
+            </Tooltip>
+
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
@@ -123,36 +120,33 @@ function MainHeader() {
           </Box>
           <Box sx={{ flexGrow: 0 }}>
             <Stack direction='row'>
-              <Tooltip title="Additional Information">
+              <Tooltip title="Options">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 1 }}>
-                  <Avatar alt="Remy Sharp" src="https://imgur.com/FPjCW0h.png" />
+                  <Avatar alt="MyAvatar" src="https://imgur.com/FPjCW0h.png" />
                 </IconButton>
               </Tooltip>
               <Stack>
                 <Typography
                   variant="h6"
                   component="h2"
-                  color={theme.palette.common.white}
+                  color={theme.palette.secondary.contrastText}
                   sx={{ display: { xs: 'none', sm: 'flex'} }}
                 >
                   Michael Leys
                 </Typography>
-                
                 <Typography
                   variant="subtitle1"
                   component="h5"
                   sx={{
                     mr: 5,
                     display: { xs: 'none', sm: 'flex' , alignSelf: 'center'},
-                  
                   }}
                 >
                   Software Engineer
                 </Typography>
               </Stack>
             </Stack>
-
-            {/* Profile Menu dropdown  */}
+            
             <Menu
               sx={{ mt: '45px' }}
               id="menu-appbar"
@@ -169,9 +163,17 @@ function MainHeader() {
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
-              {info.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+              {options.map((option) => (
+                <MenuItem sx={{ margin: 0}}>
+                  <Link 
+                    key={option.title} 
+                    href={option.link} 
+                    variant='subtitle2'
+                    underline='hover'
+                    onClick={handleCloseUserMenu} 
+                  >
+                    {option.title}
+                  </Link>
                 </MenuItem>
               ))}
             </Menu>
