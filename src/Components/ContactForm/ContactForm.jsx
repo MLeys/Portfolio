dd228b98c6c3e230873b827bbaceb5324c330672
MLeys <102929892+MLeys@users.noreply.github.com>
2023-03-20 11:29:39 -0400

    BUILD: very basic form complete and functioning

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,68 +1,81 @@
-import React, { useState } from "react";
-import { sendForm, send } from "@emailjs/browser";
-import Button from '@mui/material/Button';
-
-
+import React from 'react';
+import { useState } from 'react';
+import emailjs from 'emailjs-com';
 
 function ContactForm() {
   const [emailForm, setEmailForm] = useState({
     name: '',
     email: '',
-    message: '',
-  })
-
-  const templateParams = {
-    name: 'James',
-    notes: 'Check this out!'
-  };  
+    message: ''
+  });
 
   function handleChange(e){
-    console.log(e, '<===E')
     setEmailForm({
       ...emailForm,
       [e.target.name]: e.target.value
     });
-    console.log(emailForm, '<===EMAIL FORM')
-  }
-  // function handleSubmit(e){
-  //     send('service_8qgftwr', 'template_3rv9xth', templateParams, 'I2g4LRL1m6FvLTl89')
-  //     .then(function(response) {
-  //         console.log('SUCCESS!', response.status, response.text);
-  //     }, function(error) {
-  //         console.log('FAILED...', error);
-  //     });
-  // }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    try {
-      await send('service_8qgftwr', 'template_3rv9xth', emailForm, 'I2g4LRL1m6FvLTl89')
-      .then(function(response) {
-         console.log('SUCCESS!', response.status, response.text);
-      }, function(error) {
-         console.log('FAILED...', error);
-      });
-    } catch(err) {
-      console.log(err, ("Error sending email"))
-    }
   }
 
-
-  return ( 
-    <form id="contact-form">
-        
-        <input type="hidden" name="contact_number"></input>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" value={emailForm.name} onChange={handleChange}/>
-
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" value={emailForm.email} onChange={handleChange}/>
-
-        <label htmlFor="message">Message</label>
-        <textarea type="text" name="message" value={emailForm.message} onChange={handleChange}></textarea>
-        <Button type="submit" value="Send" onSubmit={handleSubmit}> Submit</Button>
-    </form>
-   );
+async function handleSubmit(e) {
+  e.preventDefault();
+  try {
+    console.log("HERE")
+    await emailjs.send('service_8qgftwr', 'template_3rv9xth', emailForm, 'I2g4LRL1m6FvLTl89')
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+  } catch(err) {
+    console.log(err, ("Error sending email"))
+  }
 }
 
+  return (
+    <div className="ContactForm">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Name"
+            name="name"
+            value={emailForm.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email Address"
+            name="email"
+            value={emailForm.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <textarea
+            className="form-control"
+            rows="5"
+            placeholder="Message"
+            name="message"
+            value={emailForm.message}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
 export default ContactForm;
+
+
+
