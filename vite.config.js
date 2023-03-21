affae6fa774cd804f6e0a675b21e599b56892fea
MLeys <102929892+MLeys@users.noreply.github.com>
2023-03-21 11:08:34 -0400

    DELETED server depends

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,6 @@ import react from "@vitejs/plugin-react";
 
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000'
-      }
-    }
-  },
   plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/main.jsx",
-    },
-  },
+  base: '/',
 });
